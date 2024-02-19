4ef4929801557113d67740278b038ed09f7cd527
Haru <33983955+kazuto11299@users.noreply.github.com>
2024-02-18 22:19:13 -0500

    Update ChatGPTVA.py
    Adding additional functionalities: datetime, play music, open web browser, exit.

--- ChatGPTVA.py
+++ ChatGPTVA.py
@@ -1,9 +1,11 @@
 import speech_recognition as sr
 import pyttsx3
-
+import pywhatkit
 import openai
-openai.api_key = "HIDDEN"
+import webbrowser
+import datetime
 
+openai.api_key = "Hidden"
 def speak_text(command):
     #Initialize the engine
     engine = pyttsx3.init()
@@ -27,10 +29,10 @@ def record_text():
                 print("I'm listening")
                 
                 #Listens for user's input
-                audio2 = r.listen(source2)
+                audio2 = r.listen(source2, timeout=5.0)
                 
                 #Use google to recognize audio
-                MyText = r.recognize_sphinx(audio2)
+                MyText = r.recognize_google(audio2)
                 MyText = MyText.lower()
                 
                 print(MyText)
@@ -57,14 +59,35 @@ def send_to_ChatGPT(allmessages, model = "gpt-4-1106-preview"):
     allmessages.append(reponse.choices[0].message)
     
     return message
-        
+
 messages = []
 messages.append({"role": "user", "content": "Hi I'm Alice. How can I help you?"})
 speak_text("Hi I'm Alice. How can I help you?")
 while (1):
-    text = record_text()
-    messages.append({"role": "user", "content": text})
-    response = send_to_ChatGPT(messages)
+    command = record_text()
+    messages.append({"role": "user", "content": command})
+    response = ""
+    if "open web browser" in command:
+        webbrowser.open_new_tab("www.google.com")
+        response = "Default web browser has been opened"
+
+    elif "play" in command:
+        song = command.replace("play", "")
+        response = "Playing " + song
+        pywhatkit.playonyt(song)
+
+    elif "current time" in command:
+        time = datetime.datetime.now().strftime("%I:%M %p")
+        response = "Current time is " + time
+
+    elif ("dismiss") in command:
+        response = "Goodbye!"   
+        speak_text(response)
+        exit(0)
+
+    else:   
+        response = send_to_ChatGPT(messages)
+        messages.append(response)    
     speak_text(response)
 
     print(response)
