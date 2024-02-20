4258f7d3551a1e72229f851c026a2a01d7d2fe45
trieenr <64311739+trienncg01@users.noreply.github.com>
2024-02-19 21:42:23 -0500

    Update ChatGPTVA.py
    Updates:
    Voice recognition quality
    Weather information (get city names, song names, ...)

--- ChatGPTVA.py
+++ ChatGPTVA.py
@@ -4,8 +4,11 @@ import pywhatkit
 import openai
 import webbrowser
 import datetime
+import requests
+import spacy
 
 openai.api_key = "Hidden"
+
 def speak_text(command):
     #Initialize the engine
     engine = pyttsx3.init()
@@ -22,34 +25,38 @@ def record_text():
     while (1):
         try:
             #Use the microphone as source for input
-            with sr.Microphone() as source2:
+            with sr.Microphone() as source:
                 #Prepare recognizer to receive input
-                r.adjust_for_ambient_noise(source2, duration = 0.2)
+                #r.adjust_for_ambient_noise(source, duration = 1.5)
+                #r.dynamic_energy_threshold = True
+                r.energy_threshold = 1100
                 
-                print("I'm listening")
+                print("Alice: I'm listening...")
                 
                 #Listens for user's input
-                audio2 = r.listen(source2, timeout=5.0)
+                audio = r.listen(source, timeout = 2.0)
                 
                 #Use google to recognize audio
-                MyText = r.recognize_google(audio2)
+                MyText = r.recognize_google(audio)
                 MyText = MyText.lower()
-                
-                print(MyText)
                 
                 return MyText
             
-        except sr.RequestError as e:
-            print("Could not request results: {0}", format(e))
+        # except sr.RequestError as e:
+        #     #print("Could not request results: {0}", format(e))
+        #     return None
             
-        except sr.UnknownValueError:
-            print("Unknown error occurred")
+        # except sr.UnknownValueError:
+        #     #print("Unknown error occurred")
+        #     return None
+        except:
+            continue
                 
 def send_to_ChatGPT(allmessages, model = "gpt-4-1106-preview"):
     reponse = openai.chat.completions.create(
         model = model,
         messages = allmessages,
-        max_tokens = 100,
+        max_tokens = 150,
         n = 1,
         stop = None,
         temperature = 0.5,
@@ -60,34 +67,87 @@ def send_to_ChatGPT(allmessages, model = "gpt-4-1106-preview"):
     
     return message
 
+def get_weather(api_key, city):
+    response = ""
+    
+    # API endpoint URL
+    url = f"http://api.weatherapi.com/v1/current.json?key={api_key}&q={city}"
+
+    # Send HTTP GET request to the WeatherAPI.com API
+    response = requests.get(url)
+
+    # Check if request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse JSON response
+        weather_data = response.json()
+
+        # Extract relevant weather information
+        temperature = weather_data['current']['temp_f']
+        condition = weather_data['current']['condition']['text']
+
+        # Return weather information
+        response = f"Current temperature in {city}: {temperature}°F - "
+        response += f"Condition: {condition}"
+    else:
+        # If request was not successful, print error message
+        response = "Error fetching weather data:", response.status_code
+        
+    return response
+
+def get_city_name(command: str):
+    nlp = spacy.load('en_core_web_lg')
+    doc = nlp(command)
+    for ent in doc.ents:
+        if ent.label_ == "GPE":
+            return ent
+        
+    return None
+
+def get_song_name(command: str):
+    play_index = command.find("play")
+    if play_index == -1: return None
+    return command[play_index + 5:] 
+
 messages = []
-messages.append({"role": "user", "content": "Hi I'm Alice. How can I help you?"})
-speak_text("Hi I'm Alice. How can I help you?")
+welcome_message = "Hi I'm Alice. How can I help you?"
+messages.append({"role": "system", "content": welcome_message})
+print("Alice: " + welcome_message)
+speak_text(welcome_message)
+
 while (1):
     command = record_text()
-    messages.append({"role": "user", "content": command})
+    print("User: ", command)
     response = ""
-    if "open web browser" in command:
+    
+    if ("open" and "web browser") in command:
         webbrowser.open_new_tab("www.google.com")
         response = "Default web browser has been opened"
 
     elif "play" in command:
-        song = command.replace("play", "")
+        song = get_song_name(command)
         response = "Playing " + song
         pywhatkit.playonyt(song)
 
     elif "current time" in command:
         time = datetime.datetime.now().strftime("%I:%M %p")
         response = "Current time is " + time
+        
+    elif "weather" in command:
+        weather_api = "Hidden"
+        city_name = get_city_name(command)
+        if city_name is None or len(city_name) == 0: city_name = "Cincinnati"
+        response = get_weather(weather_api, city_name)
 
     elif ("dismiss") in command:
-        response = "Goodbye!"   
+        response = "Goodbye!"
+        print("Alice: " + response)
         speak_text(response)
         exit(0)
 
     else:   
         response = send_to_ChatGPT(messages)
-        messages.append(response)    
+        messages.append({"role": "user", "content": command})
+        messages.append({"role": "system", "content": response})    
+        
+    print("Alice: " + response)
     speak_text(response)
-
-    print(response)
